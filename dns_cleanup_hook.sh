e5c329808342611373eeb54dc2805f53e1df9c8f
Samer Afach <info@afach.de>
2023-08-27 01:34:12 +0400

    Use `=` for command line arguments to support special characters at the beginning

--- dns_cleanup_hook.sh
+++ dns_cleanup_hook.sh
@@ -5,10 +5,10 @@ echo "Running auth hook cleanup for domain: $CERTBOT_DOMAIN with validation stri
 # the env var PROXY_FOR_CERTBOT_DNS_HOOK is the proxy to be used
 if [ -z "$PROXY_FOR_CERTBOT_DNS_HOOK" ]; then
     echo "No proxy provided."
-    cargo run -- --operation cleanup --domain-name $CERTBOT_DOMAIN --validation-string "$CERTBOT_VALIDATION"
+    cargo run -- --operation=cleanup --domain-name=$CERTBOT_DOMAIN --validation-string=$CERTBOT_VALIDATION
 else
     echo "Proxy provided: $PROXY_FOR_CERTBOT_DNS_HOOK"
-    cargo run -- --operation cleanup --domain-name $CERTBOT_DOMAIN --validation-string "$CERTBOT_VALIDATION" --proxy $PROXY_FOR_CERTBOT_DNS_HOOK
+    cargo run -- --operation=cleanup --domain-name=$CERTBOT_DOMAIN --validation-string=$CERTBOT_VALIDATION --proxy=$PROXY_FOR_CERTBOT_DNS_HOOK
 fi
 
 echo "Done running auth hook cleanup for domain: $CERTBOT_DOMAIN with validation string $CERTBOT_VALIDATION"
