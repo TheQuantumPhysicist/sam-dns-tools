d8904d6bb33a81a4cdf9a7c6e66ba05c47d310be
Samer Afach <info@afach.de>
2023-08-26 17:27:57 +0400

    Minor clippy fix

--- src/services/helpers.rs
+++ src/services/helpers.rs
@@ -10,7 +10,7 @@ pub fn build_client(proxy_address: Option<String>) -> reqwest::blocking::Client 
     let builder = reqwest::blocking::ClientBuilder::new();
     match proxy_address {
         Some(proxy) => builder
-            .proxy(reqwest::Proxy::all(&proxy).unwrap_or_else(|e| panic!("Invalid proxy URL: {e}")))
+            .proxy(reqwest::Proxy::all(proxy).unwrap_or_else(|e| panic!("Invalid proxy URL: {e}")))
             .build()
             .expect("Client builder with proxy failed"),
         None => builder.build().expect("Client builder failed"),
